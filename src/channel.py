3479b7266ab70de3a817a48ca65bc64ee5bfc685
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-06-07 00:26:33 +0400

    methods are filled in

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,22 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+import isodate
+
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    api_key = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id: str = channel_id
+        self.channel = None
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
